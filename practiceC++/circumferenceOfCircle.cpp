9a8772d8ad657739258faed88176d48458991d97
khushirana-05 <khushimudhad1025@gmail.com>
2024-04-30 14:48:51 +0530

    work

---
+++ practiceC++/circumferenceOfCircle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float circumference,r;
+    circumference=2*3.14*10;
+    cout<<"the circumference of circle is:"<<circumference;
+    return 0;
+}
---
+++ practiceC++/datatypeSize.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    bool b;
+    float c;
+    cout<<"size of int is: "<<sizeof(a)<<endl;
+    cout<<"size of bool is: "<<sizeof(b)<<endl;
+    cout<<"size of float is: "<<sizeof(c);
+    return 0;
+}
